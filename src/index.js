c96b239e064e190a5b1c47d4338af4924e8d22c9
Bartek Świderski <bartekzg@gmail.com>
2021-10-11 19:52:17 +0200

    popr

--- src/index.js
+++ src/index.js
@@ -1,47 +1,48 @@
 import Notiflix from "../node_modules/notiflix";
+import __defoult from '../node_modules/simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const axios = require('axios').default;
 
 const qs = (selector) => document.querySelector(selector);
 const qsa = (selector) => document.querySelectorAll(selector);
+const page = 1
+let markup = ""
+const searchBox = qs("#search-form");
+const galleryGrid = qs(".gallery");
 
-const serchBox = qs("#search-form");
-const search = qs("#search");
-let searchValue = "";
-
-const search = (event) => {
-
-  event.preventDefault();
-  searchValue = event.currentTarget.elements.searchQuery.value
-  return searchValue
-
-}
-const fetchImage = async (event) => {
-
-  event.preventDefault();
-  searchValue = event.currentTarget.elements.searchQuery.value
+async function fetchImage(searchValue) {
   const imageList = await axios.get(`https://pixabay.com/api/?key=23744712-142a310b592b893afddd0f0d4&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`);
   let arr1= [imageList.data.hits];
   let arr = [...arr1[0]]
-  return arr
+  return arr;
+  
 }
-
 
 function renderUserListItems(arr) {
-  const markup = users
-    .map(
-      (user) => `<ul class="item">
-        <p><b>Name</b>: ${user.name}</p>
-        <p><b>Email</b>: ${user.email}</p>
-        <p><b>Company</b>: ${user.company.name}</p>
-      </ul>`
-    )
-    .join("");
-  userList.innerHTML = markup;
+  arr.forEach(
+    (im) => markup +=`
+    <a  href="${im.largeImageURL}">
+    <img class="gallery__image" src="${im.webformatURL}" alt="${im.tags}" loading="lazy" />
+    </a>
+      `
+      )
+  
+      galleryGrid.innerHTML = markup;
 }
 
-serchBox.addEventListener("submit", async()=>{
- const zmi1 = await fetchImage
-  console.log(zmi1)
+
+searchBox.addEventListener("submit", async(event)=> {
+markup=""
+event.preventDefault();
+let searchValue = event.currentTarget.elements.searchQuery.value
+let arr = await fetchImage(searchValue)
+console.log(arr)
+renderUserListItems(arr)
+
 })
 
-
+let gallery = new SimpleLightbox('.gallery a',{captionsData:"alt", captionDelay:"250"});
+gallery.on('show.simplelightbox', function () {
+	preventDefault()
+});
+galleryGrid.addEventListener("click", preventDefault())
