5ec0d5a0db78a2bc04b226f117e682190c4409c0
Bartek Świderski <bartekzg@gmail.com>
2021-10-11 16:50:05 +0200

    rt

--- src/index.js
+++ src/index.js
@@ -1,9 +1,47 @@
+import Notiflix from "../node_modules/notiflix";
+const axios = require('axios').default;
+
+const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
+
+const serchBox = qs("#search-form");
+const search = qs("#search");
+let searchValue = "";
+
+const search = (event) => {
+
+  event.preventDefault();
+  searchValue = event.currentTarget.elements.searchQuery.value
+  return searchValue
+
+}
+const fetchImage = async (event) => {
+
+  event.preventDefault();
+  searchValue = event.currentTarget.elements.searchQuery.value
+  const imageList = await axios.get(`https://pixabay.com/api/?key=23744712-142a310b592b893afddd0f0d4&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`);
+  let arr1= [imageList.data.hits];
+  let arr = [...arr1[0]]
+  return arr
+}
 
 
+function renderUserListItems(arr) {
+  const markup = users
+    .map(
+      (user) => `<ul class="item">
+        <p><b>Name</b>: ${user.name}</p>
+        <p><b>Email</b>: ${user.email}</p>
+        <p><b>Company</b>: ${user.company.name}</p>
+      </ul>`
+    )
+    .join("");
+  userList.innerHTML = markup;
+}
+
+serchBox.addEventListener("submit", async()=>{
+ const zmi1 = await fetchImage
+  console.log(zmi1)
+})
 
 
-/* 
-
-https://pixabay.com/api/?key=23744712-142a310b592b893afddd0f0d4&q=dog&image_type=photo&orientation=horizontal&safesearch=true
-
-*/
